d199fad30740c2c3a13660ef65cba67b7f2c6a82
Joe Agajanian <joeagajanian@spot376.spottrading.com>
2014-10-16 12:51:08 -0500

    this calculates the prime factors of the project euler number given

---
+++ primeFactors.py
@@ -0,0 +1,22 @@
+
+
+def divider(big_number):
+    factor_list = []
+    counter = 2
+    while counter < big_number:
+        if big_number % counter == 0:
+            factor_list.append(counter)
+            big_number = big_number/counter
+        else:
+            counter = counter + 1
+    factor_list.append(big_number)
+    return factor_list
+
+
+
+def main():
+    print divider(600851475143)
+    return 0
+
+if __name__ == '__main__':
+    main()
